43828b73114e1cd8b3ce27467bfc797ef27cd923
VladasB <hulkas123@gmail.com>
2019-05-07 09:12:23 +0300

    Check php version when setting iconv encoding

--- src/MWSFinancesService/Client.php
+++ src/MWSFinancesService/Client.php
@@ -333,9 +333,11 @@ class MWSFinancesService_Client implements MWSFinancesService_Interface
      */
     public function __construct($awsAccessKeyId, $awsSecretAccessKey, $applicationName, $applicationVersion, $config = null)
     {
-        iconv_set_encoding('output_encoding', 'UTF-8');
-        iconv_set_encoding('input_encoding', 'UTF-8');
-        iconv_set_encoding('internal_encoding', 'UTF-8');
+        if(version_compare(PHP_VERSION, '5.6.0', '<')) {
+            iconv_set_encoding('output_encoding', 'UTF-8');
+            iconv_set_encoding('input_encoding', 'UTF-8');
+            iconv_set_encoding('internal_encoding', 'UTF-8');
+        }
 
         $this->_awsAccessKeyId = $awsAccessKeyId;
         $this->_awsSecretAccessKey = $awsSecretAccessKey;
